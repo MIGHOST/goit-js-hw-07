ee5f31cc1a195e6a086d92230ac9a91877bbc0b0
Mashina <mi.sidorenko@gmail.com>
2020-01-02 16:33:56 +0200

    t_2_hw_7

--- js/task_2.js
+++ js/task_2.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+];
+
+const listVegetables = document.querySelector("#ingredients");
+
+function createElement(text) {
+    const item = document.createElement("li");
+    item.textContent = text;
+    return item;
+
+}
+
+const arrListVegetables = ingredients.map(item => createElement(item))
+
+listVegetables.append(...arrListVegetables)
