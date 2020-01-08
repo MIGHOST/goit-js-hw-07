af46efad6fb3272509514eb5f0280b6e27957591
Mashina <mi.sidorenko@gmail.com>
2020-01-08 17:44:37 +0200

    t_4_hw_7

--- js/task_4.js
+++ js/task_4.js
@@ -1,1 +1,14 @@
 "use strict";
+let click = 0;
+
+let counterValue  = document.querySelector("#value");
+
+let increment = () =>  {click +=1; counterValue.textContent = click}
+
+let decrement = () => { click -=1; counterValue.textContent = click};
+
+const incrementButton = document.querySelector('[data-action="increment"]');
+const decrementButton = document.querySelector('[data-action="decrement"]');
+
+incrementButton.addEventListener("click", increment)
+decrementButton.addEventListener("click", decrement)
