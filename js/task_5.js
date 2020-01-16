e67f52341d8d0cee054ea9f7456f94c358a09727
Migho <mi.sidorenko@gmail.com>
2020-01-16 11:57:15 +0200

    fix_hw_7_t_3

--- js/task_5.js
+++ js/task_5.js
@@ -1,13 +1,14 @@
 "use strict";
 
-const inputText = document.querySelector("#name-input")
-const outputText = document.querySelector("#name-output")
+const inputText = document.querySelector("#name-input");
+const outputText = document.querySelector("#name-output");
 
 const changeName = text => {
-    if (!text.currentTarget.value.length){
-    outputText.textContent = "незнакомец";
-    return}
-    outputText.textContent=text.currentTarget.value
-} 
+    if (!text.currentTarget.value.length) {
+        outputText.textContent = "незнакомец";
+        return
+    };
+    outputText.textContent = text.currentTarget.value;
+}
 
 inputText.addEventListener("input", changeName);
